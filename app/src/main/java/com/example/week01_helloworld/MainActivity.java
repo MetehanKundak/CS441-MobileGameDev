4ff6be8e981a3d63fe5775b29e590cef033aa8ca
MetehanKundak <mkundak1@binghamton.edu>
2021-09-02 19:25:53 -0400

    Second commit

--- app/src/main/java/com/example/week01_helloworld/MainActivity.java
+++ app/src/main/java/com/example/week01_helloworld/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     TextView hello_text;
     Button  click_button;
+    Button reset_button;
     int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,22 +18,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         hello_text = (TextView) findViewById(R.id.hello_world_text);
         click_button = (Button) findViewById(R.id.button);
-
+        reset_button = (Button) findViewById(R.id.reset_button);
     }
     public void Clicked(View view) {
 
         counter++;
+        String s_counter = counter +"";
         if (counter % 2 == 1){
-            hello_text.setText("Clicked Hello world");
-            click_button.setText("Once Again");
+            hello_text.setText("You have clicked Hello world for "+counter+" times");
+            click_button.setText("Once Again to change the text");
 
         }
-        else if (counter % 2 == 0){
+        else if (counter == 0){
             hello_text.setText("Hello world");
             click_button.setText("Click");
 
         }
+        else if (counter % 2 == 0){
+            hello_text.setText("Hello Human");
+            click_button.setText("Click");
 
+        }
+
+
+    }
+
+
+
+    public void Reset(View view) {
+
+        hello_text.setText("Reset the text");
+        click_button.setText("Back to the initial state");
+        counter=0;
 
     }
 
